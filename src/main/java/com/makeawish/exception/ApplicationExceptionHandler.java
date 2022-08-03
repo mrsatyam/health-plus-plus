7310fd2561203c59a54c2cc3000c4eb73a89da8a
Satyam Joshi <satyam_joshi@epam.com>
2022-08-03 17:29:09 +0530

    One Exception handler can be used to handle multiple type of exception

--- src/main/java/com/makeawish/exception/ApplicationExceptionHandler.java
+++ src/main/java/com/makeawish/exception/ApplicationExceptionHandler.java
@@ -5,9 +5,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ApplicationExceptionHandler {
-	
 
-	@ExceptionHandler(ApplicationException.class)
+	@ExceptionHandler({ ApplicationException.class, NullPointerException.class })
 	public String handleException() {
 		System.out.println("In global Exception handler ");
 		return "error";
