d3921308b9a058685f951574cc2bacc2e94828b0
Satyam Joshi <satyam_joshi@epam.com>
2022-08-04 19:38:18 +0530

    showing user data in profile page.

--- src/main/java/com/makeawish/controller/UserProfileController.java
+++ src/main/java/com/makeawish/controller/UserProfileController.java
@@ -1,19 +1,27 @@
 package com.makeawish.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 import com.makeawish.model.LoginCredential;
+import com.makeawish.model.Users;
+import com.makeawish.repository.UserRepository;
 
 @Controller
 public class UserProfileController {
 
+	@Autowired
+	UserRepository userRepository;
+	
 	@PostMapping("/userProfile")
 	public String getUserProfile(@SessionAttribute("login") LoginCredential login, Model model) {
 		System.out.println("In User Profile Controller...");
 		System.out.println("Username from session object: " + login.getUsername());
+		Users user = userRepository.searchByUsername(login.getUsername());
+		model.addAttribute("user", user);
 		return "profile";
 	}
 }
