4f8962f75b52a06841b6fce7dd5ccb04828e00a4
Satyam Joshi <satyam_joshi@epam.com>
2022-08-08 11:21:50 +0530

    @RequestParam vs @PathVariable in REST Spring

--- src/main/java/com/makeawish/controller/ProductsRestController.java
+++ src/main/java/com/makeawish/controller/ProductsRestController.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.makeawish.model.Product;
@@ -25,5 +29,23 @@ public class ProductsRestController {
 		List<Product> products = new ArrayList<>();
 		productRepository.findAll().forEach(products::add);
 		return products;
+	}
+
+	@GetMapping("/makeawish/rest/product")
+	@ResponseBody
+	public ResponseEntity<List<Product>> getProductByRequestParam(@RequestParam("name") String name) {
+		System.out.println("Request came here");
+		List<Product> products = productRepository.searchByName(name);
+		return !products.isEmpty() ? new ResponseEntity<>(products, HttpStatus.OK)
+				: new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+	}
+
+	@GetMapping("/makeawish/rest/product/{name}")
+	@ResponseBody
+	public ResponseEntity<List<Product>> getProductByPathVariable(@PathVariable("name") String name) {
+		System.out.println("Request came here too");
+		List<Product> products = productRepository.searchByName(name);
+		return !products.isEmpty() ? new ResponseEntity<>(products, HttpStatus.OK)
+				: new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
 	}
 }
